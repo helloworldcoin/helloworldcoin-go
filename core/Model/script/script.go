9dd41d3847d4fe5ea4ab0f7867d227e842797970
邢开春 <409060350@qq.com>
2021-07-15 17:03:55 +0800

    2021071503

--- core/Model/script/script.go
+++ core/Model/script/script.go
@@ -1,7 +1,10 @@
 package script
 
+import "helloworldcoin-go/util/Stack"
+
 type VirtualMachine struct {
 }
 type Script []string
 type InputScript Script
 type OutputScript Script
+type ScriptExecuteResult Stack.Stack
