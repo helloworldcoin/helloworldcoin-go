d69aad32b73bb02976cfeda1d0f38e0fc187f78a
xingkaichun <409060350@qq.com>
2021-09-03 19:25:30 +0800

    2021090309

--- application/resources/static/js/common.js
+++ application/resources/static/js/common.js
@@ -29,14 +29,22 @@ async function $ajax(option){
 }
 
 function transactionHtml(item){
+    let {transactionInputs,transactionOutputs} = item;
+
     let left = '';
-    item.transactionInputs.forEach(item1 => {
-        left += `<div>付：<span><a title="地址，点击查看地址详情。" target="_blank" href="./address.html?address=${item1.address}">${item1.address}</a></span>&nbsp;<a title="交易输出，点击查看交易输出详情。" target="_blank" href="./transactionoutput.html?transactionHash=${item1.transactionHash}&transactionOutputIndex=${item1.transactionOutputIndex}"><i class="glyphicon-euro"></i></a>&nbsp;<span>${item1.value}</span></div>`
-    })
+    if(transactionInputs != null && transactionInputs != undefined){
+        transactionInputs.forEach(item1 => {
+            left += `<div>付：<span><a title="地址，点击查看地址详情。" target="_blank" href="./address.html?address=${item1.address}">${item1.address}</a></span>&nbsp;<a title="交易输出，点击查看交易输出详情。" target="_blank" href="./transactionoutput.html?transactionHash=${item1.transactionHash}&transactionOutputIndex=${item1.transactionOutputIndex}"><i class="glyphicon-euro"></i></a>&nbsp;<span>${item1.value}</span></div>`
+        })
+    }
+
     let right = ''
-    item.transactionOutputs.forEach(item1 => {
-        right += `<div style="display:flex">收：<span><a title="地址，点击查看地址详情。" target="_blank" href="./address.html?address=${item1.address}">${item1.address}</a></span>&nbsp;<a title="交易输出，点击查看交易输出详情。" target="_blank" href="./transactionoutput.html?transactionHash=${item1.transactionHash}&transactionOutputIndex=${item1.transactionOutputIndex}"><i class="glyphicon-euro"></i></a>&nbsp;<span>${item1.value}</span></div>`
-    });
+    if(transactionOutputs != null && transactionOutputs != undefined){
+        transactionOutputs.forEach(item1 => {
+            right += `<div style="display:flex">收：<span><a title="地址，点击查看地址详情。" target="_blank" href="./address.html?address=${item1.address}">${item1.address}</a></span>&nbsp;<a title="交易输出，点击查看交易输出详情。" target="_blank" href="./transactionoutput.html?transactionHash=${item1.transactionHash}&transactionOutputIndex=${item1.transactionOutputIndex}"><i class="glyphicon-euro"></i></a>&nbsp;<span>${item1.value}</span></div>`
+        });
+    }
+
     let transactionHtml = `
         <div style="font-size: 14px; line-height: 40px; margin-top: 10px;">
             <!-- 开头 -->
