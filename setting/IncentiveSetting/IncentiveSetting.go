82d7a6ae4d244396a6de2f03e0e9ea273d13b989
x.king <xdotking@gmail.com>
2022-06-12 11:55:40 +0800

    2022061201

--- setting/IncentiveSetting/IncentiveSetting.go
+++ setting/IncentiveSetting/IncentiveSetting.go
@@ -5,7 +5,7 @@ package IncentiveSetting
 */
 
 const BLOCK_TIME uint64 = uint64(1000) * uint64(60) * uint64(10)
-const INTERVAL_BLOCK_COUNT uint64 = uint64(6 * 24 * 7 * 2)
+const INTERVAL_BLOCK_COUNT uint64 = uint64(2)
 const INTERVAL_TIME uint64 = BLOCK_TIME * INTERVAL_BLOCK_COUNT
 const BLOCK_INIT_INCENTIVE uint64 = uint64(50) * uint64(100000000)
 const INCENTIVE_HALVE_INTERVAL uint64 = uint64(210000)
